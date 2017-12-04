05e996249fb2e6bc55935c25fe8c43c74199608c
geek <wpreul@gmail.com>
2017-12-04 15:24:07 -0600

    Support hapi 17

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ![Dr. Brule](images/brule.jpg)
 
-Dr. Brule reports on the health of your hapi server
+Dr. Brule reports on the health of your [hapi](hapijs.com) server
+
+[![Build Status](https://secure.travis-ci.org/geek/brule.svg)](http://travis-ci.org/geek/brule)
 
 `npm i brule`
 
@@ -9,12 +11,9 @@ Dr. Brule reports on the health of your hapi server
 Register with a hapi server then use `/heartbeat` or the configured path to check that the server is responsive.
 
 ```js
-const server = new Hapi.Server();
-server.connection();
+const server = Hapi.server();
 
-server.register(Brule, (err) => {
-
-});
+await server.register(Brule);
 ```
 
 ### Options
--- test/index.js
+++ test/index.js
@@ -15,49 +15,32 @@ const it = lab.it;
 const expect = Code.expect;
 
 
-it('can be registered', (done) => {
-  const server = new Hapi.Server();
-  server.connection();
-  server.register(Brule, (err) => {
-    expect(err).to.not.exist();
-    done();
-  });
+it('can be registered', async () => {
+  const server = Hapi.server();
+  await server.register(Brule);
 });
 
-it('responds with a 200 at /check-it-out', (done) => {
-  const server = new Hapi.Server();
-  server.connection();
-  server.register(Brule, (err) => {
-    expect(err).to.not.exist();
-    server.inject('/check-it-out', (res) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
+it('responds with a 200 at /check-it-out', async () => {
+  const server = Hapi.server();
+  await server.register(Brule);
+  await server.initialize();
+  const res = await server.inject('/check-it-out');
+  expect(res.statusCode).to.equal(200);
 });
 
-it('responds with a 200 at the configured path', (done) => {
+it('responds with a 200 at the configured path', async () => {
   const path = '/drbrule';
-  const server = new Hapi.Server();
-  server.connection();
-  server.register({ register: Brule, options: { path } }, (err) => {
-    expect(err).to.not.exist();
-    server.inject(path, (res) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
+  const server = Hapi.server();
+  await server.register({ plugin: Brule, options: { path } });
+  const res = await server.inject(path);
+  expect(res.statusCode).to.equal(200);
 });
 
-it('responds with a non empty response', (done) =>{
-  const server = new Hapi.Server();
-  server.connection();
-  server.register(Brule, (err) =>{
-    expect(err).to.not.exist();
-    server.inject('/check-it-out', (res) => {
-      expect(res.statusCode).to.equal(200);
-      expect(res.result).to.part.include(responses);
-      done();
-    });
-  });
+it('responds with a non empty response', async () => {
+  const server = Hapi.server();
+  await server.register(Brule);
+  await server.initialize();
+  const res = await server.inject('/check-it-out');
+  expect(res.statusCode).to.equal(200);
+  expect(res.result).to.part.include(responses);
 });
